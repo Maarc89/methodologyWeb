3a35afebba0dc0d83f63f4cc8c4463bc41e7514a
Marc <marcferreresudl@gmail.com>
2025-07-17 09:25:04 +0200

    wip

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
+import sys
 
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    # def ready(self):
+    #     if 'runserver' in sys.argv:
+    #         banner = [
+    #             "  ____ ___ __  __     ____    _    __  __ ",
+    #             " |  _ \\_ _|  \\/  |   |  _ \\  / \\  |  \\/  |",
+    #             " | |_) | || |\\/| |   | |_) |/ _ \\ | |\\/| |",
+    #             " |  __/| || |  | |   |  __// ___ \\| |  | |",
+    #             " |_|  |___|_|  |_|   |_|  /_/   \\_\\_|  |_|",
+    #             "",
+    #             "           [ API en modo desarrollo ]"
+    #         ]
+    #         for line in banner:
+    #             print(line)
